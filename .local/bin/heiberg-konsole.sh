a57cc481331fd0ad633f3d34f46a923d856babe2
Jako Heiberg <jako@heiberg.co.za>
2026-05-31 11:57:41 +0200

    chore: sync config 2026-05-31

--- .local/bin/heiberg-konsole.sh
+++ .local/bin/heiberg-konsole.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 LAYOUT_DIR="$HOME/.config/konsole/layout"
-PROJECTS=(sandbox eatingplan personalwebsite infrastructure workoutplan)
+PROJECTS=(sandbox eatingplan personalwebsite infrastructure workoutplan 17peppertree)
 
 for p in "${PROJECTS[@]}"; do
   konsole --new-tab --layout "$LAYOUT_DIR/heiberg-${p}.json" &
