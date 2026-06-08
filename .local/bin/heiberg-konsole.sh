d940c41a9aac995b8da52f5e0b59b3cd1ae59e8f
Jako Heiberg <jako@heiberg.co.za>
2026-06-08 08:28:40 +0200

    chore: sync config 2026-06-08

--- .local/bin/heiberg-konsole.sh
+++ .local/bin/heiberg-konsole.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
-konsole --tabs-from-file "$HOME/.config/konsole/heiberg-workspace.tabs"
+LAYOUT_DIR="$HOME/.config/konsole/layout"
+LAYOUTS=(heiberg-sandbox.json heiberg-eatingplan.json heiberg-personalwebsite.json heiberg-systemswebsite.json heiberg-idp.json heiberg-infrastructure.json heiberg-workoutplan.json heiberg.json)
+
+konsole --layout "$LAYOUT_DIR/${LAYOUTS[0]}" &
+KONSOLE_PID=$!
+
+# Wait until this specific Konsole window appears (up to 5s)
+for i in $(seq 50); do
+    sleep 0.1
+    wmctrl -l -p 2>/dev/null | awk '{print $3}' | grep -qx "$KONSOLE_PID" && break
+done
+
+for layout in "${LAYOUTS[@]:1}"; do
+    konsole --new-tab --layout "$LAYOUT_DIR/$layout"
+done
