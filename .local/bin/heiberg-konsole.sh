b90de2d5160a9c35fe3eccf5128f88ab47b80361
Jako Heiberg <jako@heiberg.co.za>
2026-06-08 08:18:34 +0200

    chore: sync config 2026-06-08

--- .local/bin/heiberg-konsole.sh
+++ .local/bin/heiberg-konsole.sh
@@ -1,15 +1,2 @@
 #!/usr/bin/env bash
-LAYOUT_DIR="$HOME/.config/konsole/layout"
-LAYOUTS=(heiberg-sandbox.json heiberg-eatingplan.json heiberg-personalwebsite.json heiberg-systemswebsite.json heiberg-idp.json heiberg-infrastructure.json heiberg-workoutplan.json heiberg.json)
-
-# Open first layout in a new Konsole window (no --new-tab)
-konsole --layout "$LAYOUT_DIR/${LAYOUTS[0]}" &
-sleep 1
-
-# Add remaining layouts as tabs in that new window
-for layout in "${LAYOUTS[@]:1}"; do
-  konsole --new-tab --layout "$LAYOUT_DIR/$layout" &
-  sleep 0.2
-done
-
-wait
+konsole --tabs-from-file "$HOME/.config/konsole/heiberg-workspace.tabs"
---
+++ .local/share/konsole/Workspace-HeibergSystemsWebsite.profile
@@ -0,0 +1,4 @@
+[General]
+Command=/home/jako/.local/bin/tmux-workspace.sh systemswebsite /home/jako/Development/heiberg-systems/heiberg-systems-website
+Name=Workspace-HeibergSystemsWebsite
+Parent=Jako se Profile
